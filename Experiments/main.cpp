2cf4188093ecf9d8488c4c5b2f152b29b0183b57
Padas olga <padas.oa@phystech.edu>
2022-11-13 23:39:33 +0300

    Wrote water-filling with some strange work

--- Experiments/main.cpp
+++ Experiments/main.cpp
@@ -3,7 +3,11 @@
 #include "opencv2/highgui.hpp"
 #include <opencv2/ximgproc/ridgefilter.hpp>
 
-int waterFilling(cv::Mat& dst_image, cv::Mat& src_image) {
+float inv_relu(float input_){
+	return input_ > 0 ? 0 : input_;
+}
+
+int waterFilling(cv::Mat& dst_image, cv::Mat& src_image, int max_time = 500) {
     if (src_image.size().width == 0 || src_image.size().height == 0) {
         std::cout << "Error" << "\n";
         return -1;
@@ -11,18 +15,81 @@ int waterFilling(cv::Mat& dst_image, cv::Mat& src_image) {
 
     src_image.convertTo(src_image, CV_32F);
 
-    cv::Mat height;
+    cv::Mat ground_height;
 	double scale_factor = 0.2;
-	cv::resize(src_image, height, cv::Size(), scale_factor, scale_factor, cv::INTER_LINEAR);
+	cv::resize(src_image, ground_height, cv::Size(), scale_factor, scale_factor, cv::INTER_LINEAR);
 
-    int height = height.rows;
-	int width = height.cols;
-	cv::Mat w_ = Mat(height, width, CV_32F, Scalar(0, 0, 0));
-	cv::Mat G_ = Mat(height, width, CV_32F, Scalar(0, 0, 0));
+    int height = ground_height.rows;
+	int width = ground_height.cols;
+	cv::Mat water_function = cv::Mat(height, width, CV_32FC1, cv::Scalar(0));
+	cv::Mat relief = cv::Mat(height, width, CV_32FC1, cv::Scalar(0));
 
+    double peak_value = 0;
+    float teta = 0.2;
+
+    for (int time = 0; time < max_time; ++time) {
+        relief = water_function + ground_height;
+        cv::minMaxLoc(relief, NULL, &peak_value);
+        
+        for (int x = 1; x < ground_height.cols - 2; ++x) { 
+            for (int y = 1; y < ground_height.rows - 2; ++y) {
+                double currecnt_value = relief.at<float>(x, y);
+                double pouring = exp(-time) * (peak_value - currecnt_value);
+                double delta_water = teta * 
+                    (inv_relu(-currecnt_value + relief.at<float>(x - 1, y)) +
+					inv_relu(-currecnt_value + relief.at<float>(x + 1, y)) +
+					inv_relu(-currecnt_value + relief.at<float>(x, y - 1)) +
+					inv_relu(-currecnt_value + relief.at<float>(x, y + 1)));
+                water_function.at<float>(x, y) += pouring + delta_water;
+                if (water_function.at<float>(x, y) < 0) {
+                    water_function.at<float>(x, y) = 0;
+                }
+            }
+        }
+    }
     
+	cv::Size out_size(src_image.cols, src_image.rows);
+	resize(relief, dst_image, out_size, 0, 0, cv::INTER_LINEAR);
+	dst_image.convertTo(dst_image, CV_8UC1);
+    return 0;
+}
 
-    std::cout << height.size()<< "\n";
+int incre_filling(cv::Mat& dst_image, cv::Mat& src_image, cv::Mat& original_image,
+    int max_time = 100) {
+    
+    cv::Mat ground_height = src_image;
+    ground_height.convertTo(ground_height, CV_32FC1);
+    int height = ground_height.rows; //убрать
+	int width = ground_height.cols; //убрать
+	cv::Mat water_function = cv::Mat(height, width, CV_32FC1, cv::Scalar(0));
+	cv::Mat relief = cv::Mat(height, width, CV_32FC1, cv::Scalar(0));
+
+    float teta = 0.2;
+
+    for (int time = 0; time < max_time; ++time) {
+        relief = water_function + ground_height;
+        
+        for (int x = 1; x < ground_height.cols - 2; ++x) { 
+            for (int y = 1; y < ground_height.rows - 2; ++y) {
+                double delta_water = teta * 
+                    (relief.at<float>(x - 1, y) + relief.at<float>(x + 1, y) +
+					relief.at<float>(x, y - 1) + relief.at<float>(x, y + 1) - 
+                    4. * relief.at<float>(x, y));
+                water_function.at<float>(x, y) += delta_water;
+                if (water_function.at<float>(x, y) < 0) {
+                    water_function.at<float>(x, y) = 0;
+                }
+            }
+        }
+    }
+    std::cout << relief.rows << " " << relief.cols << " " << relief.channels() << "\n";
+    std::cout << ground_height.rows << " " << ground_height.cols << " " << ground_height.channels() << "\n";
+    std::cout << src_image.rows << " " << src_image.cols << " " << src_image.channels() << "\n";
+    std::cout << original_image.rows << " " << original_image.cols << " " << original_image.channels() << "\n";
+    original_image.convertTo(original_image, CV_32F);
+    dst_image = 0.85 * original_image / relief * 255;
+    //dst_image = relief;
+    dst_image.convertTo(dst_image, CV_8UC1);
     return 0;
 }
 
@@ -32,33 +99,40 @@ int main() {
     
     std::string s = "src.png";
 
+    cv::imshow("input", image);
+    cv::waitKey(0);
+
     cv::Mat image_YCC;
     cv::cvtColor(image, image_YCC, cv::COLOR_BGR2YCrCb);
 
     cv::Mat channels[3];
     cv::split(image_YCC, channels);
-    cv::Mat channel_Y = channels[2];
+    cv::Mat channel_Y = channels[0];
 
-    //cv::imshow(s, channel_Y);
-    //cv::waitKey(0);
+    cv::imshow("Src_Y", channel_Y);
+    cv::waitKey(0);
 
-    cv::Mat dst_image;
-    waterFilling(dst_image, channel_Y);
+    cv::Mat dst_Y_image = cv::Mat(channel_Y.cols, channel_Y.rows, CV_8UC1, cv::Scalar(0));
+    waterFilling(dst_Y_image, channel_Y, 2500);
+    cv::imshow("New_Y", dst_Y_image);
+    cv::waitKey(0);
+    
+    //cv::Mat answer_Y = cv::Mat::zeros(dst_Y_image.size(), dst_Y_image.type());
+    cv::Mat answer_Y = cv::Mat(channel_Y.cols, channel_Y.rows, CV_8UC1, cv::Scalar(0));
+    incre_filling(answer_Y, dst_Y_image, channel_Y, 1);
 
-    //uint8_t Y_min = 255;
-    //uint8_t Y_max = 0;
-//
-//
-    //for (int x = 0; x < channel_Y.size[1]; ++x) {
-    //    for (int y = 0; y < channel_Y.size[0]; ++y) {
-    //        Y_max = std::max(Y_max, channel_Y.at<uint8_t>(x, y));
-    //        Y_min = std::min(Y_min, channel_Y.at<uint8_t>(x, y));
-    //    }
-    //}
-    //
-    //std::cout << (int)Y_max << " " << (int)Y_min << "\n";
+    cv::imshow("Answer_Y", answer_Y);
+    cv::waitKey(0);
 
-    //cv::imshow(s, image);
-    //cv::waitKey(0);
+    cv::Mat output_image_YCC;
+    cv::Mat output_image;
+    channels[0] = answer_Y;
+    cv::merge(channels, 3, output_image_YCC);
+
+    cv::cvtColor(output_image_YCC, output_image, cv::COLOR_YCrCb2BGR);
+    cv::imshow("Answer", output_image);
+    cv::waitKey(0);
+
+    cv::imwrite("shadow_removing.jpg", output_image);
     return 0;
 }
