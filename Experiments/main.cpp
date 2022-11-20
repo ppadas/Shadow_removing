42b6df5d650bc6c0c99afe07b700ed9fbb7b2be9
Padas olga <padas.oa@phystech.edu>
2022-11-21 00:55:01 +0300

    Fixed some errors + added water filling video

--- Experiments/main.cpp
+++ Experiments/main.cpp
@@ -7,6 +7,29 @@ float inv_relu(float input_){
 	return input_ > 0 ? 0 : input_;
 }
 
+void add_to_video(const cv::Mat& image, cv::VideoWriter& video) {
+    cv::Mat to_video_image = image;
+    to_video_image.convertTo(to_video_image, CV_8UC1);
+    cv::Mat channels[3] = {to_video_image, to_video_image, to_video_image};
+    cv::Mat relief_3channels;
+    cv::merge(channels, 3, relief_3channels);
+    cv::applyColorMap(relief_3channels, relief_3channels, cv::COLORMAP_JET);
+    video << relief_3channels;
+}
+
+bool check_converged(cv::Mat& relief, cv::Mat& previous_relief) {
+    cv::Mat diff = relief - previous_relief;
+    float delta = 0.01;
+    for (int x = 0; x < diff.cols; ++x) { 
+        for (int y = 0; y < diff.rows; ++y) {
+            if (diff.at<float>(x, y) > delta || diff.at<float>(x, y) < -delta) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 int waterFilling(cv::Mat& dst_image, cv::Mat& src_image, int max_time = 500) {
     if (src_image.size().width == 0 || src_image.size().height == 0) {
         std::cout << "Error" << "\n";
@@ -24,6 +47,8 @@ int waterFilling(cv::Mat& dst_image, cv::Mat& src_image, int max_time = 500) {
 	cv::Mat water_function = cv::Mat(height, width, CV_32FC1, cv::Scalar(0));
 	cv::Mat relief = cv::Mat(height, width, CV_32FC1, cv::Scalar(0));
 
+    cv::VideoWriter video("outcpp.avi", cv::VideoWriter::fourcc('M','J','P','G'), 50, cv::Size(width, height));
+
     double peak_value = 0;
     float teta = 0.2;
 
@@ -31,21 +56,22 @@ int waterFilling(cv::Mat& dst_image, cv::Mat& src_image, int max_time = 500) {
         relief = water_function + ground_height;
         cv::minMaxLoc(relief, NULL, &peak_value);
         
-        for (int x = 1; x < ground_height.cols - 2; ++x) { 
-            for (int y = 1; y < ground_height.rows - 2; ++y) {
-                double currecnt_value = relief.at<float>(x, y);
-                double pouring = exp(-time) * (peak_value - currecnt_value);
+        for (int x = 1; x < ground_height.rows - 2; ++x) { 
+            for (int y = 1; y < ground_height.cols - 2; ++y) {
+                double current_value = relief.at<float>(x, y);
+                double pouring = exp(-time) * (peak_value - current_value);
                 double delta_water = teta * 
-                    (inv_relu(-currecnt_value + relief.at<float>(x - 1, y)) +
-					inv_relu(-currecnt_value + relief.at<float>(x + 1, y)) +
-					inv_relu(-currecnt_value + relief.at<float>(x, y - 1)) +
-					inv_relu(-currecnt_value + relief.at<float>(x, y + 1)));
+                    (inv_relu(-current_value + relief.at<float>(x - 1, y)) +
+					inv_relu(-current_value + relief.at<float>(x + 1, y)) +
+					inv_relu(-current_value + relief.at<float>(x, y - 1)) +
+					inv_relu(-current_value + relief.at<float>(x, y + 1)));
                 water_function.at<float>(x, y) += pouring + delta_water;
                 if (water_function.at<float>(x, y) < 0) {
                     water_function.at<float>(x, y) = 0;
                 }
             }
         }
+        add_to_video(relief, video);
     }
     
 	cv::Size out_size(src_image.cols, src_image.rows);
@@ -56,7 +82,7 @@ int waterFilling(cv::Mat& dst_image, cv::Mat& src_image, int max_time = 500) {
 
 int incre_filling(cv::Mat& dst_image, cv::Mat& src_image, cv::Mat& original_image,
     int max_time = 100) {
-    
+
     cv::Mat ground_height = src_image;
     ground_height.convertTo(ground_height, CV_32FC1);
     int height = ground_height.rows; //убрать
@@ -65,12 +91,14 @@ int incre_filling(cv::Mat& dst_image, cv::Mat& src_image, cv::Mat& original_imag
 	cv::Mat relief = cv::Mat(height, width, CV_32FC1, cv::Scalar(0));
 
     float teta = 0.2;
+    int time = 0;
 
-    for (int time = 0; time < max_time; ++time) {
+    while(1) {
+        ++time;
         relief = water_function + ground_height;
-        
-        for (int x = 1; x < ground_height.cols - 2; ++x) { 
-            for (int y = 1; y < ground_height.rows - 2; ++y) {
+        cv::Mat previous_relief = relief;
+        for (int x = 1; x < ground_height.rows - 2; ++x) { 
+            for (int y = 1; y < ground_height.cols - 2; ++y) {
                 double delta_water = teta * 
                     (relief.at<float>(x - 1, y) + relief.at<float>(x + 1, y) +
 					relief.at<float>(x, y - 1) + relief.at<float>(x, y + 1) - 
@@ -81,20 +109,23 @@ int incre_filling(cv::Mat& dst_image, cv::Mat& src_image, cv::Mat& original_imag
                 }
             }
         }
+        if (check_converged(relief, previous_relief)) {
+            break;
+        }
+        if (time == 100) {
+           break;
+        }
     }
-    std::cout << relief.rows << " " << relief.cols << " " << relief.channels() << "\n";
-    std::cout << ground_height.rows << " " << ground_height.cols << " " << ground_height.channels() << "\n";
-    std::cout << src_image.rows << " " << src_image.cols << " " << src_image.channels() << "\n";
-    std::cout << original_image.rows << " " << original_image.cols << " " << original_image.channels() << "\n";
+    std::cout << time << "\n";
+
     original_image.convertTo(original_image, CV_32F);
     dst_image = 0.85 * original_image / relief * 255;
-    //dst_image = relief;
     dst_image.convertTo(dst_image, CV_8UC1);
     return 0;
 }
 
-int main() {
-    std::string path = "/home/krolchonok/Documents/Study/4_term/Polevoy/Data/example1.png";
+int main(int argc, char* argv[]) {
+    std::string path = argv[1];
     cv::Mat image = cv::imread(path, cv::IMREAD_COLOR);
     
     std::string s = "src.png";
@@ -116,10 +147,15 @@ int main() {
     waterFilling(dst_Y_image, channel_Y, 2500);
     cv::imshow("New_Y", dst_Y_image);
     cv::waitKey(0);
+
+    //cv::Mat color_map;
+    //applyColorMap(dst_Y_image, color_map, cv::COLORMAP_JET);
+    //cv::imshow("New_Y_map", color_map);
+    //cv::waitKey(0);
     
     //cv::Mat answer_Y = cv::Mat::zeros(dst_Y_image.size(), dst_Y_image.type());
     cv::Mat answer_Y = cv::Mat(channel_Y.cols, channel_Y.rows, CV_8UC1, cv::Scalar(0));
-    incre_filling(answer_Y, dst_Y_image, channel_Y, 1);
+    incre_filling(answer_Y, dst_Y_image, channel_Y, 10);
 
     cv::imshow("Answer_Y", answer_Y);
     cv::waitKey(0);
