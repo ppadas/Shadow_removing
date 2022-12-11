2f643c46f9e0f5d4e2d14a6b3d86a2a11232c936
Padas olga <padas.oa@phystech.edu>
2022-12-12 01:12:56 +0300

    Small fixes

--- Shadow_Removing/test.cpp
+++ Shadow_Removing/test.cpp
@@ -3,10 +3,11 @@
 int main(int argc, char* argv[]) {
     ShadowRemoving s;
     std::string path = argv[1];
+    std::string main_log = argv[2];
     cv::Mat image = cv::imread(path, cv::IMREAD_COLOR);
     cv::Mat dst_image;
-    s.SetLogMain("/home/krolchonok/Documents/Study/4_term/Polevoy/Shadow_removing/Shadow_Removing/build/Images/");
-    s.SetLogAll("/home/krolchonok/Documents/Study/4_term/Polevoy/Shadow_removing/Shadow_Removing/build/Images/");
+    s.SetLogMain(main_log);
+    //s.SetLogAll("/home/krolchonok/Documents/Study/4_term/Polevoy/Shadow_removing/Shadow_Removing/build/Images/");
     s.RemoveShadow(dst_image, image);
     cv::Mat shadow_mask;
     s.GetShadowMask(shadow_mask, image);
